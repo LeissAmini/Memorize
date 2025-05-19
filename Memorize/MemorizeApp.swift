b5cb5a4d160f9aa93e37f0b59d281a3581ca28a9
Leiss Amini <160181144+LeissAmini@users.noreply.github.com>
2025-05-19 09:13:38 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leiss Amini on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
