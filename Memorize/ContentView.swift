ba3f016fd7210bbe2c0b40931669a4be5203d261
Leiss Amini <160181144+LeissAmini@users.noreply.github.com>
2025-05-24 17:37:02 -0700

    togglable isFaceUp

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,24 +21,25 @@ struct ContentView: View {
 }
 
 struct CardView: View {
-    var isFaceUp : Bool = false
+    // @State lets us temporarily change faceup to change from true to false until we add our actual game logic
+    @State var isFaceUp = false
     var body: some View {
         ZStack {
+            // local variable
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 2)
+                base.fill(.white).foregroundColor(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             }else{
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
-
-
-
 
 
 
